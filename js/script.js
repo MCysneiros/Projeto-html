ece4af0e6a2c7cef1e7c90b8c0760e08f8eeda83
mcysneiros <msocys@gmail.com>
2021-08-18 17:44:15 -0300

    adicionando js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,64 @@
+// console.log("hello world");
+
+// const myName = "Matheus Cysneiros";
+// const h1 = document.querySelector(".heading-primary");
+// console.log(myName);
+// console.log(h1);
+//
+
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEL.textContent = currentYear;
+
+// mobile nav
+const btnNavEL = document.querySelector(".btn-mobile-nav");
+const headerEL = document.querySelector(".header");
+
+btnNavEL.addEventListener("click", function () {
+  headerEL.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEL.classList.toggle("nav-open");
+    }
+  });
+});
+const sectionHEroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHEroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
